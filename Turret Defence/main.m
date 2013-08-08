4069f99b338fe7336a6b2f8422b7e7b237b9c3c6
hamdanjaveed <hamdan@hamdanjaveed.com>
2013-08-08 02:51:52 -0400

    Initial Commit

---
+++ Turret Defence/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Turret Defence
+//
+//  Created by Hamdan Developer on 8/8/13.
+//  Copyright Hamdan Javeed 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
